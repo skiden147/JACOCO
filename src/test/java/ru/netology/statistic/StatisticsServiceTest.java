38b11433202d4bfbd3d21ebb8aecf9342ec54ba4
Denis Skidan <skidan147@gmail.com>
2023-07-18 17:11:17 +0300

    JaCoCo added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxAgain() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
